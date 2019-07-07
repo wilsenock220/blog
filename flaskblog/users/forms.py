cbd6451b0525d3909df56621ae3336aa0ae75918
wilsenock220 <wilsenock220@gmail.com>
2019-07-07 21:47:36 +0300

    change structure to blueprints

--- flaskblog/users/forms.py
+++ flaskblog/users/forms.py
@@ -55,13 +55,6 @@ class UpdateAccountForm(FlaskForm):
             if user:
                 raise ValidationError('That email is taken. Please choose a different one.')
 
-
-class PostForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    content = TextAreaField('Content', validators=[DataRequired()])
-    submit = SubmitField('Post')
-
-
 class RequestResetForm(FlaskForm):
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
