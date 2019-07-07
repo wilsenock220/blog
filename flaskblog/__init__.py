3e2de8c66f9b5d241871b26bbc54e64a4b6cd4b7
wilsenock220 <wilsenock220@gmail.com>
2019-07-07 21:14:52 +0300

    add fixed email issue

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -14,11 +14,11 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+app.config['MAIL_USERNAME'] = 'wilsenock220@gmail.com'
+app.config['MAIL_PASSWORD'] = '126yzr10382'
 mail = Mail(app)
 
 
